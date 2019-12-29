2016a49bf441786b665b262b87c0bfbf326a9dfe
rainy-me <github@rainy.me>
2019-12-30 02:18:43 +0900

    add orbtk

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use orbtk::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+      Application::new()
+        .window(|ctx| {
+            Window::create()
+                .title("Tweak")
+                .position((100.0, 100.0))
+                .size(420.0, 730.0)
+                .child(TextBlock::create().text("OrbTk").build(ctx))
+                .build(ctx)
+        })
+        .run();
 }
